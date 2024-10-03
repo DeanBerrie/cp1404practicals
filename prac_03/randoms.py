6e88b074b97ff41f97f3fddf38caabc68aa0a119
deanberrie <deanberrie@gmail.com>
2024-10-04 06:55:13 +1000

    Add questions and answers about different random functions

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+"""
+What did you see on line 1? On line 1 I ran it 3 times and got 11, 15, and 20.
+However I could have got any number between 5-20
+
+What did you see on line 2? I ran the statment multiple times and got 3,5,7, and 9. This is because
+the range starts at 3 and only includes every 2nd number up to 10.
+
+What did you see on line 3? when ran I got the floats 4.1, 4.86, and 3.8. Uniform prints the floats of a
+number between the parametres in this case 2.5-5.5
+
+"""
+
+      )
