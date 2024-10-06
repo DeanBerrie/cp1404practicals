c90897cab03df5babd1da502293f978e8e18fe14
deanberrie <deanberrie@gmail.com>
2024-10-06 14:34:19 +1000

    Add string formatting to prac 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,8 +1,23 @@
-input_name = input("Name: ")
-out_file = open('name.txt', 'w')
-print(input_name, file=out_file)
-out_file.close()
+# input_name = input("Name: ")
+# out_file = open('name.txt', 'w')
+# print(input_name, file=out_file)
+# out_file.close()
+#
+# infile = open('name.txt', 'r')
+# read_name = infile.read()
+# print(f'Hi {read_name.strip()}!')
+# infile.close()
 
-infile = open('name.txt', 'r')
-read_name = infile.read()
-print(f'Hi {read_name.strip()}!')
+# with open('numbers.txt', "r") as infile:
+#     first_number = infile.readline()
+#     second_number = infile.readline()
+#     final_number = first_number + second_number
+#     print(final_number) #TODO
+
+total = 0
+with open('numbers.txt', 'r') as number_infile:
+    for line in number_infile:
+        number = int(line)
+        total = total + number
+    print(f'The total of all numbers are {total}')
+
