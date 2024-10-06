f57bb8daebb3fbb3cb3c92dacd3ac55ae7063500
deanberrie <deanberrie@gmail.com>
2024-10-06 14:54:48 +1000

    Add password checker requirements

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,7 +5,7 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 6
-IS_SPECIAL_CHARACTER_REQUIRED = False
+IS_SPECIAL_CHARACTER_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -28,20 +28,31 @@ def main():
 def is_valid_password(password):
     """Determine if the provided password is valid."""
     # TODO: if length is wrong, return False
-
+    if len(password) > MAX_LENGTH or len(password) < MIN_LENGTH:
+        return False
     number_of_lower = 0
     number_of_upper = 0
     number_of_digit = 0
     number_of_special = 0
     for character in password:
         # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        if character.isdigit():
+            number_of_digit += 1
+        elif character.isupper():
+            number_of_upper += 1
+        elif character.islower():
+            number_of_lower += 1
+        else:
+            number_of_special += 1
 
     # TODO: if any of the 'normal' counts are zero, return False
-
+    if number_of_lower == 0 or number_of_digit == 0 or number_of_upper == 0:
+        return False
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
-
+    if IS_SPECIAL_CHARACTER_REQUIRED:
+        if number_of_special == 0:
+            return False
     # if we get here (without returning False), then the password must be valid
     return True
 
