f5d2043b91c402f079f8e66b9ecbd5b0fc8cc67a
deanberrie <deanberrie@gmail.com>
2024-10-06 18:32:47 +1000

    Fix program #3 and label all programs

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,23 +1,31 @@
+# 1
+
 # input_name = input("Name: ")
 # out_file = open('name.txt', 'w')
 # print(input_name, file=out_file)
 # out_file.close()
-#
+
+# 2
+
 # infile = open('name.txt', 'r')
 # read_name = infile.read()
 # print(f'Hi {read_name.strip()}!')
 # infile.close()
 
-# with open('numbers.txt', "r") as infile:
-#     first_number = infile.readline()
-#     second_number = infile.readline()
-#     final_number = first_number + second_number
-#     print(final_number) #TODO
+# 3
 
-total = 0
-with open('numbers.txt', 'r') as number_infile:
-    for line in number_infile:
-        number = int(line)
-        total = total + number
-    print(f'The total of all numbers are {total}')
+with open('numbers.txt', "r") as infile:
+    first_number = int(infile.readline())
+    second_number = int(infile.readline())
+    final_number = first_number + second_number
+    print(final_number)
 
+
+# 4
+
+# total = 0
+# with open('numbers.txt', 'r') as number_infile:
+#     for line in number_infile:
+#         number = int(line)
+#         total = total + number
+#     print(f'The total of all numbers are {total}')
