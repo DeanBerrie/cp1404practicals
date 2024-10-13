b3d7cefb5c04b725d402c38e8e80cb4925c6c9df
deanberrie <deanberrie@gmail.com>
2024-10-13 11:31:31 +1000

    Add function to display subjects with string formatting

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,26 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subjects = load_data()
+    print_data(subjects)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject.append(parts)
     input_file.close()
+    return subject
+
+
+def print_data(subjects):
+    for subject in subjects:
+        print(f'{i[0]} is taught by {i[1]} and has {i[2]} students')
 
 
 main()
