96446ddb05e4205018357b73f2ee4fbe840c24d2
deanberrie <deanberrie@gmail.com>
2024-10-13 10:51:52 +1000

    Add predictions for string outputs

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,24 +1,13 @@
-"""
-CP1404/CP5632 Practical
-Starter code for cumulative total income program
-"""
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Q1: numbers[0] will print the number 3, the first index
+# Q2: numbers[-1] will print the number 2, the last index
+# Q3: numbers[3] will print the number 1, the 4th index
+# Q4: numbers[:-1] will print all numbers but the last index
+# Q5: numbers[3:4] will just print the 4th index which is 1
+# Q6: 5 in numbers will search the list and return true if the number 5 is in the list
+# Q7: 7 in numbers will search the list and return true if the number 7 is in the list, it will return false
+# Q8: "3" in numbers will search the list and return true if the string "3" is in the list, it will return false
+# Q9: numbers + [6, 5, 3] will temporarily add the numbers onto the end of the current list
 
 
-def main():
-    """Display income report for incomes over a given number of months."""
-    incomes = []
-    months = int(input("How many months? "))
-
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
-        incomes.append(income)
-
-    print("\nIncome Report\n-------------")
-    total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
-        total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-
-
-main()
