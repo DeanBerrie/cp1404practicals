1b83a7f6d6da5ed0a1168e41449cc26de31c4d5e
deanberrie <deanberrie@gmail.com>
2024-10-20 12:41:03 +1000

    Add docstring to program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,10 +1,15 @@
+"""
+Hex colours
+Prac - 05
+"""
+
 COLOUR_TO_HEX_CODE = {"Aqua": "#00ffff", "Fawn": "#e5aa70", "Keppel": "#3", "Linen": "#faf0e6", "Mantis": "#74c365",
                       "Orchid": "#da70d6", "Ruby": "#e0115f", "Sienna": "a0522d", "Teal": "#008080", "Zaffre": "0014a8"}
 
 user_colour = input("Enter Colour: ").title()
 while user_colour != "":
     try:
-        print(user_colour, "is", COLOUR_TO_HEX_CODE[user_colour])
+        print(f'the code for {user_colour}, is, {COLOUR_TO_HEX_CODE[user_colour]}')
     except KeyError:
         print("Invalid colour")
     user_colour = input("Enter Colour: ").title()
