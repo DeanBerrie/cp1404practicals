52af135671c41ca7a5324654a77c9d3dfac048fa
deanberrie <deanberrie@gmail.com>
2024-11-03 11:15:54 +1000

    add dynamic typed language print statement

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -14,5 +14,11 @@ def main():
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
 
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are: ")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.language)
+
 
 main()
