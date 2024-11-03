ad28fad9693113b48a23cd77a59ede2f90a7ca9b
deanberrie <deanberrie@gmail.com>
2024-11-03 11:49:46 +1000

    Add guitars program

---
+++ prac_06/guitars.py
@@ -0,0 +1,36 @@
+"""
+CP1404 Prac 6
+Guitars
+Estimated time: 10 minutes
+Time taken: 13 minutes
+"""
+
+from guitar import Guitar
+
+
+def main():
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f'{guitar} added.')
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("Guitars: ")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars")
+
+main()
