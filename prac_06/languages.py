baa041d7250bf14e3bfd2ddb897d840f374bbe28
deanberrie <deanberrie@gmail.com>
2024-11-03 11:12:21 +1000

    add languages program

---
+++ prac_06/languages.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Prac 6
+Languages
+Estimated time: 20 minutes
+Time taken:
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
