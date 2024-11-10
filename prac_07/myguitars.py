7171ca8785064afc734fae25ad3eb9bee52574a0
deanberrie <deanberrie@gmail.com>
2024-11-10 13:49:51 +1000

    refactor into functions

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -3,11 +3,7 @@ from guitar import Guitar
 
 def main():
     guitars = []
-    with open('guitars.csv', "r", encoding="utf-8-sig") as infile:
-        for line in infile:
-            parts = line.strip().split(",")
-            guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-            guitars.append(guitar)
+    get_guitars(guitars)
 
     print("My guitars!")
     name = input("Name: ")
@@ -19,9 +15,21 @@ def main():
         print(f'{guitar} added.')
         name = input("Name: ")
 
+    write_to_file(guitars)
+
+
+def write_to_file(guitars):
     with open("guitars.csv", "w", encoding="utf-8-sig") as outfile:
         for guitar in sorted(guitars):
             print(guitar, file=outfile)
+
+
+def get_guitars(guitars):
+    with open('guitars.csv', "r", encoding="utf-8-sig") as infile:
+        for line in infile:
+            parts = line.strip().split(",")
+            guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+            guitars.append(guitar)
 
 
 main()
