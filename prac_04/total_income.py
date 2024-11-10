bc71ae565829050c7090f6176feb234478c3f570
deanberrie <deanberrie@gmail.com>
2024-11-10 12:54:17 +1000

    add information into README

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -5,7 +5,6 @@ Starter code for cumulative total income program
 
 
 def main():
-    """Display income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
 
@@ -17,6 +16,7 @@ def main():
 
 
 def print_report(incomes, number_of_months):
+    """Print report and accumulating total"""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
