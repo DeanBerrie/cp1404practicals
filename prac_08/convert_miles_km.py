aae92d63c6973708daf3c407a4173e87ff807efe
deanberrie <deanberrie@gmail.com>
2024-11-17 13:08:25 +1000

    add dynamic programs

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,29 +1,34 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 MILES_TO_KILOMETRE = 1.60934
 
 
 class MilesToKilometreApp(App):
+    output_km = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculate(self):
-        value = self.get_valid_miles()
-        result = value * MILES_TO_KILOMETRE
-        self.root.ids.output_label.text = str(result)
+    def handle_calculate(self, text):
+        value = self.get_valid_miles(text)
+        self.update_result(value)
 
-    def get_valid_miles(self):
+    @staticmethod
+    def get_valid_miles(text):
         try:
-            value = float(self.root.ids.input_miles.text)
-            return value
+            return float(text)
         except ValueError:
-            return 0
+            return 0.0
 
-    def handle_increment(self, change):
-        value = self.get_valid_miles() + change
+    def update_result(self, miles):
+        self.output_km = str(miles * MILES_TO_KILOMETRE)
+
+    def handle_increment(self, text, change):
+        value = self.get_valid_miles(text) + change
         self.root.ids.input_miles.text = str(value)
 
 
